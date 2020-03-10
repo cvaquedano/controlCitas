c2be245f6a4725347173d6e1858ec1c4a11e64a9
Carlos Vaquedano <chvb2002@hotmail.com>
2020-03-10 13:46:06 -0600

    use of local stora

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import Formulario from './components/Formulario'
+import Cita from './components/Cita'
 
 
 function App() {
 
-  const [citas, guardarCitas] = useState([]);
+
+  //citas en local storage
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if(!citasIniciales){
+    citasIniciales = [];
+  }
+
+// Arreglo de citas
+  const [citas, guardarCitas] = useState(citasIniciales);
+
+
+  useEffect(()=>{
+    let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+    if(citasIniciales){
+      localStorage.setItem('citas',JSON.stringify(citas));
+    } else{
+      localStorage.setItem('citas',JSON.stringify([]));
+    }
+
+  },[citas]);
 
   const crearCita = cita => {
-    guardarCitas([
-      ...citas,
-      cita
-    ]);
+    guardarCitas([...citas, cita]);
   }
+
+  const eliminarCita= id =>{
+     const nuevasCitas = citas.filter(cita => cita.id !==id);
+     guardarCitas(nuevasCitas);
+
+  }
+
+  const titulo = citas.length === 0 ? "No hay citas": "Administra tu citas";
   return (
     <Fragment>
        <h1>Administradr de pacientes</h1>
@@ -22,7 +47,14 @@ function App() {
              crearCita={crearCita}/>
            </div>
            <div className="one-half column">
-             2
+  <h2>{titulo}</h2>
+             {citas.map(cita=>(
+               <Cita
+               key={cita.id}
+               cita={cita}
+               eliminarCita={eliminarCita}
+               />
+             ))}
            </div>
 
          </div>
